1b59e10b8bce8777cd2ef5b1fca9ae57a8718acf
Henjo Hoeksma <hphoeksma@stylence.nl>
2013-05-16 13:34:39 +0200

    [TASK] Initial commit

---
+++ ext_tables.php
@@ -0,0 +1,56 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Simply Responsive Content');
+
+$tmp_columns = array(
+	'responsive_touch' => array (
+		'exclude' => 0,
+		'label' => 'LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:responsive_touch',
+		'config' => array(
+			'type' => 'radio',
+			'items' => array (
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:default', ''),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:hide_for_touch', 'hide-for-touch'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:show_for_touch', 'show-for-touch'),
+			),
+			'default' => 0,
+		),
+	),
+	'responsive_show' => array(
+		'exclude' => 0,
+		'label' => 'LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:responsive_show',
+		'config' => array(
+			'type' => 'radio',
+			'items' => array (
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:default', ''),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:show_for_small', 'show-for-small'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:show_for_medium', 'show-for-medium'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:show_for_large', 'show-for-large'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:show_for_xlarge', 'show-for-xlarge'),
+			),
+			'default' => 0,
+		),
+	),
+	'responsive_hide' => array(
+		'exclude' => 0,
+		'label' => 'LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:responsive_hide',
+		'config' => array(
+			'type' => 'radio',
+			'items' => array (
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:default', ''),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:hide_for_small', 'hide-for-small'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:hide_for_medium', 'hide-for-medium'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:hide_for_large', 'hide-for-large'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:hide_for_xlarge', 'hide-for-xlarge'),
+			),
+			'default' => 0,
+		),
+	),
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tmp_columns, 1);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', '--div--;LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:responsive, responsive_touch, responsive_show, responsive_hide');
+?>
---
+++ ext_tables.sql
@@ -0,0 +1,8 @@
+#
+# Table structure for table 'tt_content'
+#
+CREATE TABLE tt_content (
+	responsive_touch varchar(255) DEFAULT '' NOT NULL,
+	responsive_show varchar(255) DEFAULT '' NOT NULL,
+	responsive_hide varchar(255) DEFAULT '' NOT NULL,
+);
