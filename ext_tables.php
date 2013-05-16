016ae579c64845f359d5b49959910b76735714de
Henjo Hoeksma <hphoeksma@stylence.nl>
2013-05-16 14:30:09 +0200

    [TASK] Adding orientation classes and full hide & show classes

--- ext_tables.php
+++ ext_tables.php
@@ -27,8 +27,12 @@ $tmp_columns = array(
 			'items' => array (
 				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:default', ''),
 				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:show_for_small', 'show-for-small'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:show_for_medium_down', 'show-for-medium-down'),
 				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:show_for_medium', 'show-for-medium'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:show_for_medium_up', 'show-for-medium-up'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:show_for_large_down', 'show-for-large-down'),
 				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:show_for_large', 'show-for-large'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:show_for_large_up', 'show-for-large-up'),
 				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:show_for_xlarge', 'show-for-xlarge'),
 			),
 			'default' => 0,
@@ -42,9 +46,28 @@ $tmp_columns = array(
 			'items' => array (
 				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:default', ''),
 				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:hide_for_small', 'hide-for-small'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:hide_for_medium_down', 'hide-for-medium-down'),
 				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:hide_for_medium', 'hide-for-medium'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:hide_for_medium_up', 'hide-for-medium-up'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:hide_for_large_down', 'hide-for-large-down'),
 				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:hide_for_large', 'hide-for-large'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:hide_for_large_up', 'hide-for-large-up'),
 				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:hide_for_xlarge', 'hide-for-xlarge'),
+			),
+			'default' => 0,
+		),
+	),
+	'responsive_orientation' => array(
+		'exclude' => 0,
+		'label' => 'LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:responsive_orientation',
+		'config' => array(
+			'type' => 'radio',
+			'items' => array (
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:default', ''),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:show_for_landscape', 'show-for-landscape'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:show_for_portrait', 'show-for-portrait'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:hide_for_landscape', 'hide-for-landscape'),
+				array('LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:hide_for_portrait', 'hide-for-portrait'),
 			),
 			'default' => 0,
 		),
@@ -52,5 +75,5 @@ $tmp_columns = array(
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tmp_columns, 1);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', '--div--;LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:responsive, responsive_touch, responsive_show, responsive_hide');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', '--div--;LLL:EXT:simply_responsive_content/Resources/Private/Language/locallang_db.xlf:responsive, responsive_touch, responsive_orientation, responsive_show, responsive_hide');
 ?>
--- ext_tables.sql
+++ ext_tables.sql
@@ -5,4 +5,5 @@ CREATE TABLE tt_content (
 	responsive_touch varchar(255) DEFAULT '' NOT NULL,
 	responsive_show varchar(255) DEFAULT '' NOT NULL,
 	responsive_hide varchar(255) DEFAULT '' NOT NULL,
+	responsive_orientation varchar(255) DEFAULT '' NOT NULL,
 );
